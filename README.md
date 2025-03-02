8646909b436d3b412000516b8ab41a16bd0a39c8
Techaphatr INDHAVIVADHANA <techaphatr.i@ku.th>
2025-03-03 03:28:31 +0700

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Tech Camp #1
-
-## Build docker image
-
-```bash
-docker build . -t tech-camp
-```
-
-## Run docker image
-
-```bash
-docker run -p 3000:3000 --name tech-camp tech-camp
-```
+# JobFair 2025 Countdown
--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,41 @@ var socket = io();
 var mainVideo = document.getElementById("main-video");
 var secondaryVideo = document.getElementById("secondary-video");
 
+document.getElementById("cont").style.display = "none";
+
+const targetDate = new Date('2025-03-05T17:00:00+07:00');
+
+function load() {
+  let now = new Date();
+  const result = now.toLocaleDateString('th-TH', {
+      timeZone: 'Asia/Bangkok',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+  });
+  document.getElementById("date").textContent = result;
+
+  let difference = targetDate - now;
+
+  // Calculate the remaining time in days, hours, minutes, and seconds
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  // Add leading zeros if needed
+  days = (days < 10 ? "0" : "") + days;
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  // Display the countdown
+  document.getElementById("time1").textContent = days;
+  document.getElementById("time2").textContent = hours;
+  document.getElementById("time3").textContent = minutes;
+  document.getElementById("time4").textContent = seconds;
+}
+
 socket.on('index', (check) => {
   // stop video
   secondaryVideo
@@ -10,6 +45,14 @@ socket.on('index', (check) => {
     secondaryVideo.play();
     secondaryVideo.addEventListener("ended", function () {
       secondaryVideo.style.display = "none";
+
+      load()
+      document.getElementById("cont").style.display = "flex";
+
+      setInterval(() => {
+        load()
+      }, 1000);
+
     });
   }
 });
